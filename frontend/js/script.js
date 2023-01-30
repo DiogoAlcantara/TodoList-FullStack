9733faa019329fb1c8d116f3964ba16038d2b5af
Diogo Alcantara <diogo.alcantara1997@gmail.com>
2023-01-30 19:45:28 -0300

    finalizando o projeto

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -9,6 +9,7 @@ const fetchTasks = async () => {
   return tasks;
 };
 
+// adiciona um nova task quando o formulário é enviado
 const addTask = async (event) => {
   event.preventDefault();
   const task = { title: inputTask.value };
@@ -21,6 +22,35 @@ const addTask = async (event) => {
 
   loadTasks();
   inputTask.value = "";
+};
+
+// deleta as tasks quando ocorre um clique no botão
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:3333/tasks/${id}`, {
+    method: "delete",
+  });
+
+  loadTasks();
+};
+
+const updateTask = async ({ id, title, status }) => {
+  await fetch(`http://localhost:3333/tasks/${id}`, {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, status }),
+  });
+
+  loadTasks();
+};
+
+// formata a data que vem do banco, para o horário local do usuário
+const formatDate = (dateUTC) => {
+  const options = {
+    dateStyle: "long",
+    timeStyle: "short",
+  };
+  const date = new Date(dateUTC).toLocaleString("pt-br", options);
+  return date;
 };
 
 // função que cria as tags HTML
@@ -55,10 +85,20 @@ const createRowTask = (task) => {
   // criando a estrutura da tabela
   const tr = createElement("tr");
   const tdTitle = createElement("td", title);
-  const tdCreatedAt = createElement("td", created_at);
+  const tdCreatedAt = createElement("td", formatDate(created_at));
   const tdStatus = createElement("td");
   const tdActions = createElement("td");
   const select = createSelect(status);
+  select.addEventListener("change", ({ target }) =>
+    updateTask({ ...task, status: target.value })
+  );
+
+  const editForm = createElement("form");
+  const editInput = createElement("input");
+
+  editInput.value = title;
+  editForm.appendChild(editInput);
+
   const editButton = createElement(
     "button",
     "",
@@ -81,6 +121,19 @@ const createRowTask = (task) => {
   editButton.classList.add("btn-action");
   deleteButton.classList.add("btn-action");
 
+  deleteButton.addEventListener("click", () => deleteTask(id));
+
+  editForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    updateTask({ id, title: editInput.value, status: status });
+  });
+
+  editButton.addEventListener("click", () => {
+    tdTitle.innerText = "";
+    tdTitle.appendChild(editForm);
+  });
+
   // adicionando o onteúdo a cada linha da tabela
   tr.appendChild(tdTitle);
   tr.appendChild(tdCreatedAt);
@@ -93,6 +146,7 @@ const createRowTask = (task) => {
 // carrega as tarefas existentes na tabela tasks
 const loadTasks = async () => {
   const tasks = await fetchTasks();
+  tbody.innerHTML = "";
 
   tasks.forEach((task) => {
     const tr = createRowTask(task);
